8b11b49c4643787b72b5350e22e06c8996f7aebc
student <student@student-10.local>
2018-04-05 12:49:58 +0300

    так ну это моя про игра

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -9,8 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var TREE: UIImageView!
+    @IBOutlet weak var collect: UILabel!
+    @IBOutlet weak var score: UILabel!
+    
+    @IBAction func buttentup(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
+    var listOfWords = [
+    "Игорь",
+    "Акинфеев",
+        "БОГ"
+    
+    ]
+    let incorrectMovesAllowed = 8
+    var totalWins = 0
+    var totalLoses = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    newRound()
     }
+    
+    var currentGame: Game!
+    
+    func newRound() {
+    let newWord = listOfWords.removeFirst()
+    currentGame = Game(word: newWord, incorectMovesRemaining: incorrectMovesAllowed)
+    }
+    
 }
